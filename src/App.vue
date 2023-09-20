e23de010c0c5378c9111ba3ad58fcf195fb98a9b
olegblackwall <olegblackwall@mail.ru>
2023-09-21 00:45:15 +0700

    #003 restructing AddTodoList/ add f addTodo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
   <AppFilters />
 
   <main class="app-main">
-    <AppTodoList />
+    <AppTodoList 
+      :todos="todos" 
+      @toggle-todo="toggleTodo" 
+      @remove-todo="removeTodo" 
+    />
     <AppAddTodo @add-todo="addTodo"/>
   </main>
 
@@ -19,6 +23,10 @@ import AppAddTodo from "./components/AppAddTodo.vue"
 import AppFooter from "./components/AppFooter.vue"
 import { Todo } from "./types/Todo"
 
+interface State {
+  todos: Todo[]
+}
+
 export default defineComponent({
   components: {
     AppHeader,
@@ -27,9 +35,31 @@ export default defineComponent({
     AppAddTodo,
     AppFooter
   },
+  data(): State {
+    return {
+      todos: [
+        { id: 0, text: 'Learn the basics of Vue', completed: true },
+        { id: 1, text: 'Learn the basics of Typescript', completed: false },
+        { id: 2, text: 'Subscribe to the channel', completed: false }
+      ]
+    }
+  },
   methods: {
     addTodo(todo: Todo) {
-      console.log(todo)
+      this.todos.push(todo)
+    },
+    toggleTodo(id: number) {
+      // Ищем в массиве todos нужный объект todo по id
+      // (todo: Todo) - говорит что todo это объект типа Todo. Можно написать просто todo.
+      const targetTodo = this.todos.find((todo: Todo) => todo.id === id)
+      // Если targetTodo найдено, то свойство completed переключится на противоположное значение
+      if(targetTodo) {
+        targetTodo.completed = !targetTodo.completed
+      }
+    },
+    removeTodo(id: number) {
+      // Новый массив будет состоять из элементов, чей id не совпадает с id аргумента
+      this.todos = this.todos.filter((todo: Todo) => todo.id !== id)
     }
   }
 })
--- src/components/AppTodoList.vue
+++ src/components/AppTodoList.vue
@@ -11,41 +11,30 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { PropType, defineComponent } from 'vue'
 import AppTodoItem from './AppTodoItem.vue'
 import { Todo } from '../types/Todo'
-
-interface State {
-  todos: Todo[]
-}
 
 export default defineComponent({
   components: {
     AppTodoItem
   },
-  data(): State {
-    return {
-      todos: [
-        { id: 0, text: 'Learn the basics of Vue', completed: true },
-        { id: 1, text: 'Learn the basics of Typescript', completed: false },
-        { id: 2, text: 'Subscribe to the channel', completed: false }
-      ]
+  props: {
+    todos: {
+      type: Array as PropType<Todo[]>
     }
   },
   methods: {
     toggleTodo(id: number) {
-      // Ищем в массиве todos нужный объект todo по id
-      // (todo: Todo) - говорит что todo это объект типа Todo. Можно написать просто todo.
-      const targetTodo = this.todos.find((todo: Todo) => todo.id === id)
-      // Если targetTodo найдено, то свойство completed переключится на противоположное значение
-      if(targetTodo) {
-        targetTodo.completed = !targetTodo.completed
-      }
+      this.$emit('toggleTodo', id)
     },
     removeTodo(id: number) {
-      // Новый массив будет состоять из элементов, чей id не совпадает с id аргумента
-      this.todos = this.todos.filter((todo: Todo) => todo.id !== id)
+      this.$emit('removeTodo', id)
     }
+  },
+  emits: {
+    toggleTodo: (id: number) => Number.isInteger(id),
+    removeTodo: (id: number) => Number.isInteger(id)
   }
 })
 </script>
