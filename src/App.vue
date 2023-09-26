b6bcffcbfc3f8b0e82ec08d34a12d8f981183d78
olegblackwall <olegblackwall@mail.ru>
2023-09-27 00:40:20 +0700

    #005 app done

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <AppHeader />
-  
-  <AppFilters :active-filter="activeFilter" @set-filter="setFilter"/>
+
+  <AppFilters :active-filter="activeFilter" @set-filter="setFilter" />
 
   <main class="app-main">
-    <AppTodoList 
-      :todos="filteredTodos" 
-      @toggle-todo="toggleTodo" 
+    <AppTodoList
+      :todos="filteredTodos"
+      @toggle-todo="toggleTodo"
       @remove-todo="removeTodo"
     />
-    <AppAddTodo @add-todo="addTodo"/>
+    <AppAddTodo @add-todo="addTodo" />
   </main>
 
-  <AppFooter />
+  <AppFooter :stats="stats"/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
-import AppHeader from "./components/AppHeader.vue"
-import AppFilters from "./components/AppFilters.vue"
-import AppTodoList from "./components/AppTodoList.vue"
-import AppAddTodo from "./components/AppAddTodo.vue"
-import AppFooter from "./components/AppFooter.vue"
-import { Todo } from "./types/Todo"
-import { Filter } from "./types/filter"
+import { defineComponent } from "vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppFilters from "./components/AppFilters.vue";
+import AppTodoList from "./components/AppTodoList.vue";
+import AppAddTodo from "./components/AppAddTodo.vue";
+import AppFooter, { Stats } from "./components/AppFooter.vue";
+import { Todo } from "./types/Todo";
+import { Filter } from "./types/filter";
 
 interface State {
-  todos: Todo[],
-  activeFilter: Filter
+  todos: Todo[];
+  activeFilter: Filter;
 }
 
 export default defineComponent({
@@ -36,51 +36,63 @@ export default defineComponent({
     AppFilters,
     AppTodoList,
     AppAddTodo,
-    AppFooter
+    AppFooter,
   },
   data(): State {
     return {
       todos: [
-        { id: 0, text: 'Learn the basics of Vue', completed: true },
-        { id: 1, text: 'Learn the basics of Typescript', completed: false },
-        { id: 2, text: 'Subscribe to the channel', completed: false }
+        { id: 0, text: "Learn the basics of Vue", completed: true },
+        { id: 1, text: "Learn the basics of Typescript", completed: false },
+        { id: 2, text: "Subscribe to the channel", completed: false },
       ],
-      activeFilter: 'All'
-    }
+      activeFilter: "All",
+    };
   },
   computed: {
     filteredTodos(): Todo[] {
       switch (this.activeFilter) {
-        case 'Active':
-          return this.todos.filter(todo => !todo.completed)
-        case 'Done':
-          return this.todos.filter(todo => todo.completed)
-        case 'All':
+        case "Active":
+          return this.activeTodos
+        case "Done":
+          return this.doneTodos
+        case "All":
         default:
-          return this.todos
+          return this.todos;
       }
+    },
+    stats(): Stats {
+      return {
+        active: this.activeTodos.length,
+        done: this.doneTodos.length
+      }
+    },
+    activeTodos(): Todo[] {
+        return this.todos.filter((todo) => !todo.completed)
+    },
+    doneTodos(): Todo[] {
+        return this.todos.filter((todo) => todo.completed)
     }
   },
   methods: {
     addTodo(todo: Todo) {
-      this.todos.push(todo)
+      this.todos.push(todo);
     },
     toggleTodo(id: number) {
       // Ищем в массиве todos нужный объект todo по id
       // (todo: Todo) - говорит что todo это объект типа Todo. Можно написать просто todo.
-      const targetTodo = this.todos.find((todo: Todo) => todo.id === id)
+      const targetTodo = this.todos.find((todo: Todo) => todo.id === id);
       // Если targetTodo найдено, то свойство completed переключится на противоположное значение
-      if(targetTodo) {
-        targetTodo.completed = !targetTodo.completed
+      if (targetTodo) {
+        targetTodo.completed = !targetTodo.completed;
       }
     },
     removeTodo(id: number) {
       // Новый массив будет состоять из элементов, чей id не совпадает с id аргумента
-      this.todos = this.todos.filter((todo: Todo) => todo.id !== id)
+      this.todos = this.todos.filter((todo: Todo) => todo.id !== id);
     },
     setFilter(filter: Filter) {
-      this.activeFilter = filter
-    }
-  }
-})
+      this.activeFilter = filter;
+    },
+  },
+});
 </script>
